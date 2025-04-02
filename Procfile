00752954f428a40e94ba11e5827785b0bb616037
Sardlimad <genesis.david.08@gmail.com>
2025-04-02 16:28:09 -0400

    Cambios Prod 7

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn waitx.wsgi:application --bind 0.0.0.0:$PORT 
+web: cd waitx && gunicorn waitx.wsgi:application --bind 0.0.0.0:$PORT 
--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ echo "Configurando PYTHONPATH..."
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 echo "Recolectando archivos estáticos..."
-python waitx/manage.py collectstatic --no-input
+cd waitx && python manage.py collectstatic --no-input
 
 echo "Aplicando migraciones..."
-python waitx/manage.py migrate
+python manage.py migrate
 
 echo "Build completado con éxito!" 
